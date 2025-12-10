4eda932114201125f7c56b488fabacd676038aa9
Pranav6862 <pranavp6238@gmail.com>
2025-12-10 18:22:49 +0530

    Updated word counter code

--- task1word_counter-python.py
+++ task1word_counter-python.py
@@ -1,20 +1,20 @@
 
 
 def count_words(input_string):
-    count_char =len(input_string)
+    count_char = len(input_string)
     
-    vowels="aeiouAEIOU"
-    vowel_count=0
+    vowels = "aeiouAEIOU"
+    vowel_count = 0
 
     for ch in input_string:
         if ch in vowels:
-            vowel_count +=1
+            vowel_count += 1
 
-    word_count =len(input_string.split())
+    word_count = len(input_string.split())
     return count_char, vowel_count, word_count
     
 
-input_string=input("Enter a string:")
+input_string = input("Enter a string:")
 chars, vowels, words = count_words(input_string)
 
 print(f"Number of character: {chars}")
